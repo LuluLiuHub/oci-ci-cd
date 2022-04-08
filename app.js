140e42abd74873c65334d1e2fa87b2489addd02f
Lulu Liu <lulu.liu@oracle.com>
2022-04-08 15:16:36 -0500

    change it to my version

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World, yall!');
+  res.send('Hello World, LuluHub!');
 });
 
 app.listen(PORT, HOST);
